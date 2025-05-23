689f440505485a378c32dabc803f248a9eecde4c
YBJ0000 <95595902+YBJ0000@users.noreply.github.com>
2025-05-23 13:36:05 +1000

    build initial app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from 'react'
-import './App.css'
+
+const data = {
+  vegetables: ['Carrot', 'Cucumber'],
+  spices: ['Salt', 'Pepper', 'Chilli', 'Herbs', 'Curry'],
+  fruits: ['Apple', 'Banana', 'Pear', 'Watermelon', 'Grape', 'Strawberry', 'Mango', 'Blackberry']
+}
 
 function App() {
+  const [activeTab, setActiveTab] = useState(null)
+  const [selectedItem, setSelectedItem] = useState(null)
 
   return (
     <>
-
+      <h1>Inventory</h1>
+      <Tabs
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+        data={data}
+        selectedItem={selectedItem}
+        setSelectedItem={setSelectedItem}
+      />
     </>
   )
 }
